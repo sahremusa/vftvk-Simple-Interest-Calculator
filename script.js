4a1c4a3b0c5964adc4b42a6e654c551fe379fca4
sahremusa <48286632+sahremusa@users.noreply.github.com>
2021-02-11 08:04:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() { 
+    var p = document.getElementById("principal").value;
+    var n = document.getElementById("years").value;
+    var r = document.getElementById("rate").value;
     
+    var result = p * n * r;
+        var inrst = result / 100;
+        var interest = document.getElementById("SI").innerHTML="Simple Interest is" +inrst;
 }
+
+
         
